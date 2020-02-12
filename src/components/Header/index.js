253fe57793f361e9b311cdf1e8007c8a0b2bef10
Victor Siqueira da Cunha <codeakio@Mac-mini-de-Victor.local>
2020-02-11 23:09:18 -0300

    Redux, Add product to cart, Cart product quantity and Reactotron

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,7 +8,7 @@ import { Container, Cart } from './styles';
 
 import logo from '../../assets/images/logo.svg';
 
-const index = () => {
+function Header({ cartSize }) {
   return (
     <Container>
       <Link to="/">
@@ -17,12 +18,14 @@ const index = () => {
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>3 itens</span>
+          <span>{cartSize} itens</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
-};
+}
 
-export default index;
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
